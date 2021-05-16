4d12886adbc16dd171a53ebf51e24e20d6782936
yyyzzz674 <yz624557674@163.com>
2021-05-16 19:03:13 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhuo
+ * @Date: 2021-05-16 17:56:19
+ * @LastEditTime: 2021-05-16 18:05:28
+ */
+module.exports = {
+  base: '/yz_blog/',
+  title: 'yyyzzz674',
+  description: '个人博客'
+}
